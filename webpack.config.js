fd9188b1d82960e76942338e72008c0fe68bc1da
Simon Hudson <hellosimonhudson+github@gmail.com>
2019-02-25 14:46:58 +0000

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-const outputDirectory = 'dist';
+const outputDirectory = 'public';
 
 module.exports = {
     entry: ['babel-polyfill', './src/client/index.js'],
