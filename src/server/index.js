9cd1adc0e7731a43efedeaea8e92891ec6554ecd
Simon Hudson <hellosimonhudson+github@gmail.com>
2019-02-25 14:37:34 +0000

    server update

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-if (process.env.NODE_ENV === 'production') app.use(express.static('../../public'));
+if (process.env.NODE_ENV === 'production') app.use(express.static('public'));
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../../public', 'index.html'));
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
